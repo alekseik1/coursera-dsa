704cae51bb888830ca9185f698411f96198b2f82
alekseik1 <1alekseik1@gmail.com>
2023-01-21 16:47:41 +0300

    feat(toolbox-w1): hw-1

---
+++ algorithmic-toolbox/week-1/hw-1/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by aleksei on 1/21/23.
+//
+#include <iostream>
+
+int main() {
+    int first_number, second_number;
+    std::cin >> first_number >> second_number;
+    std::cout << first_number + second_number;
+    return 0;
+}
